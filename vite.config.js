9a3900d16ac61811bca7dd745f974a099bfb4f34
adadarsh23 <adarshkumr004@gmail.com>
2025-09-17 00:18:19 +0530

    Profile Website

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from "path"
+import path from "path";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer({ open: true }),],
   server: {
     open: true,
   },
@@ -14,5 +15,23 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  chunkSizeWarningLimit: 1000,
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // All node_modules go into vendor chunk
+            return 'vendor';
+          }
+        }
+      }
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,     // Removes console.log statements
+        drop_debugger: true,   // Removes debugger statements
+      }
+    }
+  }
 });
