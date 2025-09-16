b4b9c6526450e6ebecc097edb52ca17c0bc25773
adadarsh23 <adarshkumr004@gmail.com>
2025-09-17 00:01:49 +0530

    Profile Website

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styles from "../Modules/bubble.module.css";
 
 export default function About() {
+  const BubbleText = () => (
+    <h2 className="text-center text-3xl md:text-6xl font-thin text-white bg-black p-4 md:p-6 rounded-lg mb-8 md:mb-16">
+      {"About Us".split("").map((char, idx) => (
+        <span className={styles.hoverText} key={idx}>
+          {char}
+        </span>
+      ))}
+    </h2>
+  );
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold">About Page</h1>
+    <div className="flex flex-col items-center justify-center mt-10 md:mt-20 px-4 md:px-0">
+      <BubbleText />
+      <div className="relative w-full max-w-7xl">
+
+      </div>
     </div>
   );
 }
