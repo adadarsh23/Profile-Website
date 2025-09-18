cfdf0355e88bddceb8eb78733a4ab49fe7727ca7
adadarsh23 <adarshkumr004@gmail.com>
2025-09-18 08:49:04 +0530

    Profile Website

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Modules/Loading.module.css';
+import './Loading.module.css';
 
 export default function Loading() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,15 @@ import path from "path";
 import { visualizer } from 'rollup-plugin-visualizer';
 import compression from 'vite-plugin-compression';
 import { VitePWA } from 'vite-plugin-pwa';
+import ViteSitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer({ open: true }), compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 1024 }),VitePWA({ registerType: 'autoUpdate', workbox: { globPatterns: ['**/*.{js,css,html,svg,png,woff2}'] } })
-],
+  plugins: [react(), tailwindcss(), visualizer({ open: true }), compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 1024 }), VitePWA({ registerType: 'autoUpdate', workbox: { globPatterns: ['**/*.{js,css,html,svg,png,woff2}'] } }),
+  ViteSitemap({
+    hostname: 'https://adadarsh23.netlify.app',
+  })
+  ],
   server: {
     open: true,
   },
@@ -24,10 +28,9 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-             if (id.includes('react')) return 'vendor_react'
+            if (id.includes('react')) return 'vendor_react'
             if (id.includes('lodash')) return 'vendor_lodash'
             if (id.includes('framer-motion')) return 'vendor_framer-motion'
-
             // All node_modules go into vendor chunk
             return 'vendor';
           }
